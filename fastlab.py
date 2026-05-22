f65c84e08f5cf60671643b2ebd53375a0c6d3ad3
Wolframetal <dimonvolk111@gmail.com>
2026-05-22 12:43:36 +0700

    checking unit-tests

---
+++ fastlab.py
@@ -0,0 +1,13 @@
+import uvicorn
+app = FastAPI()
+
+def sum_two_args(x,y):
+    return x+y
+# Hello World route
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
